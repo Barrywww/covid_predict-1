4e2d2f31b909a1172e62f3966b9b2c83a4aec102
brilliantTya <fabulous0tya@gmail.com>
2020-12-01 17:29:29 +0800

    commit

--- preprocess_owid.py
+++ preprocess_owid.py
@@ -16,28 +16,43 @@ template = pd.DataFrame(index=iso, columns=date)
 
 def generate_constant_data():
     # get the cosntant values using the mean of all the country data
-    grouped_mean = df.groupby(['iso_code']).mean()
-
-    columns = []
-    for col in ['population', 'population_density', 'median_age', 'aged_65_older', 'aged_70_older', 'gdp_per_capita', 'extreme_poverty', 'cardiovasc_death_rate', 'diabetes_prevalence', 'female_smokers', 'male_smokers', 'handwashing_facilities', 'hospital_beds_per_thousand', 'life_expectancy', 'human_development_index']:
-        columns.append(grouped_mean[col])
-    constant_values = pd.concat(columns, axis=1)
+    constant_values = df.groupby(['iso_code']).mean().drop(columns=['total_cases', 'new_cases', 'new_cases_smoothed', 'total_deaths', 'new_deaths', 'new_deaths_smoothed', 'total_cases_per_million', 'new_cases_per_million', 'new_cases_smoothed_per_million', 'total_deaths_per_million',
+                                                                    'new_deaths_per_million', 'new_deaths_smoothed_per_million', 'total_tests', 'new_tests', 'total_tests_per_thousand', 'new_tests_per_thousand', 'new_tests_smoothed', 'new_tests_smoothed_per_thousand', 'tests_per_case', 'positive_rate'])
+    for col in constant_values:
+        if constant_values[col].count() < 150:
+            constant_values = constant_values.drop(columns=[col])
+    regime = _integrate_regime_data()
+    constant_values = pd.concat(
+        [regime, constant_values], axis=1, join="outer")
+    constant_values.sort_index(inplace=True)
     constant_values.to_csv("preprocessed_data/constant_values.csv")
 
 
+def _integrate_regime_data():
+    political_regime = pd.read_csv('data/political-regime-updated2016.csv')
+    regime2015 = political_regime[political_regime['Year'] == 2015]
+    regime2015 = regime2015.set_index('iso_code')
+    regime2015 = regime2015.drop(columns=['Entity', 'Year'])
+    return regime2015
+
+
 # for every feature, use this function to throw the values into the template
-def fill_owid_data(col_name):
+def _fill_owid_data(col_name):
     this = template.copy()
     for index, row in df.iterrows():
         this.loc[row['iso_code'], row['date']] = row[col_name]
     return this
 
 
-def generate_owid_csv(col_name):
-    this = fill_owid_data(col_name)
+def _generate_owid_csv(col_name):
+    this = _fill_owid_data(col_name)
     this.to_csv("preprocessed_data/" + col_name + ".csv")
 
 
+def generate_owid_csv():
+    for col in ['total_cases', 'new_cases', 'total_deaths', 'new_deaths', 'total_cases_per_million', 'new_cases_per_million', 'total_deaths_per_million', 'new_deaths_per_million', 'total_tests', 'new_tests', 'positive_rate', 'stringency_index']:
+        generate_owid_csv(col)
+
+
+# generate_owid_csv()
 generate_constant_data()
-for col in ['total_cases', 'new_cases', 'total_deaths', 'new_deaths', 'total_cases_per_million', 'new_cases_per_million', 'total_deaths_per_million', 'new_deaths_per_million', 'total_tests', 'new_tests', 'positive_rate', 'stringency_index']:
-    generate_owid_csv(col)

1ea720b0b54750b594d1f0922c0f945a3263dde2
brilliantTya <fabulous0tya@gmail.com>
2020-12-07 15:53:01 +0800

    commit

--- democracy_index/process2.py
+++
@@ -1,7 +0,0 @@
-out = ''
-with open('bundle2.txt') as f:
-    for line in f:
-        line = line.replace(" ", ',')
-        out = out + line
-with open('../data/democracy_index.csv', 'a') as target:
-    target.write(out)

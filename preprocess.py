8a84b2857e69e16a928e1072e0a7e44576dd968d
brilliantTya <fabulous0tya@gmail.com>
2020-12-09 20:44:19 +0800

    commit

--- preprocess.py
+++ preprocess.py
@@ -2,17 +2,18 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-variable_list = ['total_cases', 'new_cases', 'new_cases_smoothed', 'total_deaths', 'new_deaths', 'new_deaths_smoothed', 'total_cases_per_million', 'new_cases_per_million', 'new_cases_smoothed_per_million','total_deaths_per_million', 'new_deaths_per_million', 'new_deaths_smoothed_per_million', 'total_tests', 'new_tests', 'total_tests_per_thousand', 'new_tests_per_thousand', 'new_tests_smoothed', 'new_tests_smoothed_per_thousand', 'tests_per_case', 'positive_rate']
+variable_list = ['new_cases', 'total_deaths', 'total_tests', 'tests_per_case', 'positive_rate']
 
 # Extracting features
 def prepare_data():
-    owid_data = pd.read_csv("./data/owid-covid-data.csv", dtype=str).drop(columns=['continent', 'location', 'stringency_index', 'tests_units'])
+    owid_data = pd.read_csv("./data/owid-covid-data.csv", dtype=str).drop(columns=['continent', 'location', 'stringency_index', 'tests_units', 'weekly_icu_admissions', 'weekly_hosp_admissions', 'hosp_patients', 'total_cases', 'new_deaths', 'new_tests', 'icu_patients'])
+    owid_data.drop(columns=[c for c in owid_data.columns if c.lower()[-7:] == 'million' or c.lower()[-8:] == 'smoothed' or c.lower()[-8:] == 'thousand'], inplace=True)
+
     oxf_data = pd.read_csv("./data/OxCGRT_latest.csv", dtype=str)
     oxf_data = oxf_data[oxf_data["Jurisdiction"] == "NAT_TOTAL"]
     oxf_data.drop(columns=['CountryName', 'RegionName', 'RegionCode', 'Jurisdiction', 'M1_Wildcard', 'ConfirmedCases', 'ConfirmedDeaths'], inplace=True)
     oxf_data.rename(columns={'E2_Debt/contract relief': 'E2_Debt contract relief', 'Date': 'date', 'CountryCode': 'iso_code'}, inplace=True)
-    cols = [c for c in oxf_data.columns if c.lower(
-    )[-4:] != 'flag' and c.lower()[-7:] != 'display']
+    cols = [c for c in oxf_data.columns if c.lower()[-4:] != 'flag' and c.lower()[-7:] != 'display']
     oxf_data = oxf_data[cols]
 
     # Setting datatype
@@ -30,17 +31,18 @@ def prepare_data():
         by=['iso_code'], inplace=True)
 
     owid_variables = owid_data.loc[:, variable_list]
+    
 
     oxf_data = oxf_data[oxf_data["date"] >= pd.Timestamp(2020, 1, 23)]
 
     return owid_data, oxf_data, owid_constant_features, owid_variables
 
 
-def generate_country_list(df1, df2):
+def generate_country_list(series1, series2):
     # iso = (pd.unique(df['iso_code'].dropna())).tolist()
     # iso = [code for code in iso if code[:4] != 'OWID']
     # return iso
-    return sorted(list(set(df1).intersection(set(df2))))
+    return sorted(list(set(series1).intersection(set(series2))))
 
 
 def generate_date_series():
@@ -50,13 +52,17 @@ def generate_date_series():
 def generate_country_csv():
     owid_data, oxf_data, owid_constant_features, owid_variables = prepare_data()
     country_list = generate_country_list(owid_data["iso_code"], oxf_data["iso_code"])
-    owid_data = owid_data[owid_data["iso_code"].isin(country_list)].copy()
-    oxf_data = oxf_data[oxf_data["iso_code"].isin(country_list)].copy()
-    world_data = pd.merge(owid_data, oxf_data, how='outer').set_index('date')
+    # owid_data = owid_data[owid_data["iso_code"].isin(country_list)].copy()
+    # oxf_data = oxf_data[oxf_data["iso_code"].isin(country_list)].copy()
+    world_data = pd.merge(owid_data, oxf_data, how='inner').set_index('date')
 
     for country in tqdm(country_list):
         this_df = world_data[world_data["iso_code"] == country].drop(columns=['iso_code']).sort_values(by=['date'])
-        this_df.to_csv("./country_csv/" + country + ".csv")
+        this_train = this_df.iloc[: 252]
+        this_test = this_df.iloc[252:]
+        this_train.to_csv("./country_csv/train/" + country + "_train.csv")
+        this_test.to_csv("./country_csv/test/" + country + "_test.csv")
 
 
-generate_country_csv()
+if __name__ == '__main__':
+    generate_country_csv()
